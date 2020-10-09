7b6f7057bd3f6db106af0cbd6eb4a4e9812eb6d2
kreutzoga <kreutzoga@gmail.com>
2020-10-09 13:33:15 +0200

    Update parseUnit with cooldown

--- unit.cpp
+++ unit.cpp
@@ -1,7 +1,7 @@
 #include "unit.h"
 
-Unit::Unit(const std::string name, const int hp, const int dmg)
-	: name(name), hp(hp), dmg(dmg) {}
+Unit::Unit(const std::string name, const int hp, const int dmg, const double cd)
+	: name(name), hp(hp), dmg(dmg), cd(cd) {}
 
 std::string Unit::getName() const
 {
@@ -18,21 +18,31 @@ int Unit::getDmg() const
 	return dmg;
 }
 
+double Unit::getCd() const
+{
+	return cd;
+}
+
 void Unit::fight(Unit &other)
 {
 	while (hp > 0)
 	{
 		(other.getHp() - dmg > 0) ? other.hp -= dmg : other.hp = 0;
 
-		if (other.getHp() == 0) { break; }
+		if (other.getHp() == 0)
+		{
+			break;
+		}
 
 		(hp - other.getDmg() > 0) ? hp -= other.dmg : hp = 0;
 	}
 
-	if (other.getHp() == 0) {
+	if (other.getHp() == 0)
+	{
 		std::cout << name << " wins. Remaining HP: " << hp << std::endl;
 	}
-	else {
+	else
+	{
 		std::cout << other.getName() << " wins. Remaining HP: " << other.getHp() << std::endl;
 	}
 }
@@ -42,33 +52,44 @@ Unit Unit::parseUnit(std::string fileName)
 	std::string name;
 	int hp = 0;
 	int dmg = 0;
+	double cd = 0;
 
 	std::ifstream file("units/" + fileName);
 
-	if (!file.fail() && file.is_open()) {
+	if (!file.fail() && file.is_open())
+	{
 		std::string line;
-		while (std::getline(file, line)) {
+		while (std::getline(file, line))
+		{
 			int startPos = line.find_first_of(":") + 2;
 
-			if (line.find("name") != std::string::npos) {
+			if (line.find("name") != std::string::npos)
+			{
 				name = line.substr(startPos + 1, line.length() - (startPos + 3));
 			}
-			else if (line.find("hp") != std::string::npos) {
+			else if (line.find("hp") != std::string::npos)
+			{
 				hp = std::stoi(line.substr(startPos, line.length() - (startPos + 1)));
 			}
-			else if (line.find("dmg") != std::string::npos) {
+			else if (line.find("dmg") != std::string::npos)
+			{
 				dmg = std::stoi(line.substr(startPos, line.length() - startPos));
+			}
+			else if (line.find("cd") != std::string::npos)
+			{
+				cd = std::stod(line.substr(startPos, line.length() - startPos));
 			}
 		}
 	}
-	else {
+	else
+	{
 		throw std::runtime_error("Error while opening the file: " + fileName);
 	}
 
-	return Unit(name, hp, dmg);
+	return Unit(name, hp, dmg, cd);
 }
 
 std::ostream &operator<<(std::ostream &out, const Unit &u)
 {
-	return out << u.getName() << ": HP: " << u.getHp() << ", DMG: " << u.getDmg() << "\n";
+	return out << u.getName() << ": HP: " << u.getHp() << ", DMG: " << u.getDmg() << ", CD: " << u.getCd() << "\n";
 }
--- unit.h
+++ unit.h
@@ -14,10 +14,11 @@ private:
 	const double cd;
 
 public:
-	Unit(const std::string name, const int hp, const int dmg);
+	Unit(const std::string name, const int hp, const int dmg, const double cd);
 	std::string getName() const;
 	int getHp() const;
 	int getDmg() const;
+	double getCd() const;
 	void fight(Unit &other);
 	static Unit parseUnit(const std::string fileName);
 	friend std::ostream &operator<<(std::ostream &out, const Unit &u);
