8fe2bfa132744b096eb59e84c6f9d9a4e36a06d0
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-10-15 12:18:06 +0200

    Add unit test source code

---
+++ test/parser_test.cpp
@@ -0,0 +1,53 @@
+#include "../jsonparser.h"
+#include <gtest/gtest.h>
+
+TEST(parserTest, test_iStream)
+{
+    std::ifstream iStream("units/unit_2.json");
+    std::map<std::string, std::string> expectedData{
+        {"name", "Sally"},
+        {"hp", "45"},
+        {"dmg", "30"}};
+    std::map<std::string, std::string> data = JSONParser::Parse(iStream);
+
+    for (auto e : expectedData)
+    {
+        ASSERT_EQ(data[e.first], e.second);
+    }
+}
+
+TEST(parserTest, test_iString)
+{
+    std::string iString = "units/unit_1.json";
+    std::map<std::string, std::string> expectedData{
+        {"name", "Maple"},
+        {"hp", "150"},
+        {"dmg", "10"}};
+    std::map<std::string, std::string> data = JSONParser::Parse(iString);
+
+    for (auto e : expectedData)
+    {
+        ASSERT_EQ(data[e.first], e.second);
+    }
+}
+
+TEST(parserTest, test_invalidInput)
+{
+    const std::string expectedError = "Invalid key: nev";
+    std::ifstream invalidInput("units/invalid_unit.json");
+    try
+    {
+        std::map<std::string, std::string> data = JSONParser::Parse(invalidInput);
+        FAIL() << "Expected std::runtime_error";
+    }
+    catch (std::runtime_error &e)
+    {
+        ASSERT_EQ(e.what(), expectedError);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
