0b0063ff6d95857c732b1c82396fbfd599b33778
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-10-15 11:26:00 +0200

    Fix file path in testing script

--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,9 @@
 touch test/results.txt 
 > test/results.txt
 
-./a.out unit_1.json unit_2.json >> test/results.txt
-./a.out unit_1.json unit_3.json >> test/results.txt
-./a.out unit_2.json unit_1.json >> test/results.txt
-./a.out unit_2.json unit_3.json >> test/results.txt
-./a.out unit_3.json unit_1.json >> test/results.txt
-./a.out unit_3.json unit_2.json >> test/results.txt
+./a.out units/unit_1.json units/unit_2.json >> test/results.txt
+./a.out units/unit_1.json units/unit_3.json >> test/results.txt
+./a.out units/unit_2.json units/unit_1.json >> test/results.txt
+./a.out units/unit_2.json units/unit_3.json >> test/results.txt
+./a.out units/unit_3.json units/unit_1.json >> test/results.txt
+./a.out units/unit_3.json units/unit_2.json >> test/results.txt
