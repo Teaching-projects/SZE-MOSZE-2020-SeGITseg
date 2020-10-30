07871a61eed25f6ac7cc8421640aa0782513faa0
kreutzoga <kreutzoga@gmail.com>
2020-10-30 11:11:07 +0100

    Add documentation to jsonparser.h

--- jsonparser.h
+++ jsonparser.h
@@ -1,3 +1,17 @@
+/**
+* \class Jsonparser
+*
+* \brief This is the Jsonparser class, which is used to input JSON files.
+*
+* \author Boros Dávid, Káldy Kristóf, Kreutzog András 
+*
+* \version 1.0
+*
+* \date 2020/10/15 17:30
+*
+* Created on: 2020/10/15 17:30
+*/
+
 #ifndef JSONPARSER_H
 #define JSONPARSER_H
 
@@ -10,6 +24,7 @@ class JSONParser
 private:
     static bool isNumeric(const std::string &input);
     static std::string getData(const std::string &line);
+
 public:
     static std::map<std::string, std::string> ParseStream(std::istream &inputStream);
     static std::map<std::string, std::string> ParseString(const std::string &inputString);
