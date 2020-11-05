89e720f3fccb466b4b4d8704ea8ae9571cbb3bd3
kreutzoga <kreutzoga@gmail.com>
2020-11-05 11:05:09 +0100

    Create new variables

--- Hero.h
+++ Hero.h
@@ -3,7 +3,14 @@
 
 class Hero : public Monster
 {
-    
+private:
+    float XpPerLevel;
+    float DamageBonusPerLevel;
+    float HpBonusPerLevel;
+    float CdMultiplierPerLevel;
+public:
+    Hero();
+    static Hero parse(const std::string&);
 };
 
 #endif
