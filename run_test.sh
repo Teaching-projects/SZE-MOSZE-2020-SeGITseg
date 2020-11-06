60041f8eb6e45d817b9aade5aee9935f1bc2f5b9
kreutzoga <kreutzoga@gmail.com>
2020-11-06 15:09:36 +0100

    Change scenario path

--- run_test.sh
+++ run_test.sh
@@ -6,5 +6,5 @@ touch test/results1.txt
 touch test/results2.txt 
 > test/results2.txt
 
-./game scenario1.json >> test/results1.txt
-./game scenario2.json >> test/results2.txt
+./game units/scenarios/scenario1.json >> test/results1.txt
+./game units/scenarios/scenario2.json >> test/results2.txt
