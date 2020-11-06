80f50f5458f2754995520b6c4979cc4feff17619
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-11-06 13:53:14 +0100

    Update Hero parser

--- Hero.cpp
+++ Hero.cpp
@@ -2,10 +2,10 @@
 #include "JSON.h"
 #include <iostream>
 
-Hero::Hero(const std::string& name, int hp, int dmg, double cd, const float XpPerLevel, const float HpBonusPerLevel, const float DamageBonusPerLevel, const float CdMultiplierPerLevel)
-    : Monster{ name,hp,dmg,cd }, maxHP(hp), xp(0), lvl(1), XpPerLevel(XpPerLevel), HpBonusPerLevel(HpBonusPerLevel), DamageBonusPerLevel(DamageBonusPerLevel), CdMultiplierPerLevel(CdMultiplierPerLevel) {}
+Hero::Hero(const std::string &name, int hp, int dmg, double cd, const float XpPerLevel, const float HpBonusPerLevel, const float DamageBonusPerLevel, const float CdMultiplierPerLevel)
+    : Monster{name, hp, dmg, cd}, maxHP(hp), xp(0), lvl(1), XpPerLevel(XpPerLevel), HpBonusPerLevel(HpBonusPerLevel), DamageBonusPerLevel(DamageBonusPerLevel), CdMultiplierPerLevel(CdMultiplierPerLevel) {}
 
-void Hero::addXp(const int& dmg)
+void Hero::addXp(const int &dmg)
 {
     xp += dmg;
     while (xp >= XpPerLevel)
@@ -24,7 +24,7 @@ void Hero::lvlUp()
     dmg += HpBonusPerLevel;
 }
 
-int Hero::attack(Monster& target)
+int Hero::attack(Monster &target)
 {
     int dmgPerRound = 0;
     dmgPerRound = Monster::attack(target);
@@ -47,7 +47,17 @@ int Hero::getMaxHealthPoints() const
     return maxHP;
 }
 
-Hero Hero::parse(const std::string& fileName)
+Hero Hero::parse(const std::string &fileName)
 {
+    JSON data = JSON::parseFromFile(fileName);
 
+    return Hero(
+        data.get<std::string>("name"),
+        data.get<int>("base_health_points"),
+        data.get<int>("base_damage"),
+        data.get<double>("base_attack_cooldown"),
+        data.get<float>("experience_per_level"),
+        data.get<float>("health_point_bonus_per_level"),
+        data.get<float>("damage_bonus_per_level"),
+        data.get<float>("cooldown_multiplier_per_level"));
 }
